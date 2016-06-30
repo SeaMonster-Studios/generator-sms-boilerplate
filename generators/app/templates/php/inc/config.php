f862472c7fb606396973b9bb319572e2a9c2b364
Nick Harberg <nick@seamonsterstudios.com>
2016-06-30 13:47:25 -0700

    fixing pathways

--- generators/app/templates/php/inc/config.php
+++ generators/app/templates/php/inc/config.php
@@ -3,5 +3,5 @@
 $server_url = $_SERVER["SERVER_NAME"];
 $doc_root = $_SERVER["DOCUMENT_ROOT"];
 
-echo $url = $server_url.'/'. $some_relative_path."<br />";
-echo $dir = $doc_root.'/'. $some_relative_path;
+// echo $url = $server_url.'/'. $some_relative_path."<br />";
+// echo $dir = $doc_root.'/'. $some_relative_path;
